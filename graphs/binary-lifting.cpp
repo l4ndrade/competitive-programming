ed431f410b852196a2a3114f60f2bb019fa881a4
Leonardo Porto <103781072+l4ndrade@users.noreply.github.com>
2025-08-17 20:09:48 -0300

    Changes in the binary lifting code

--- graphs/binary-lifting.cpp
+++ graphs/binary-lifting.cpp
@@ -6,29 +6,32 @@ vector<vector<int>> adj;
 struct Binary_Lifting
 {
     int n; // number of nodes
-    int l; // log(n)
-    vector<vector<int>> ancestor;
-    vector<int> tin, tout;
+    int l; // ceil(log(n))+1
+    vector<vector<int>> ancestor; // ancestor[u][k] = 2^k-th ancestor of u
+    vector<int> tin; // tin[u] = t when the dfs call in u started
+    vector<int> tout; // tout[u] = t when the dfs call in u ended
     int t; // Timter
 
     // loads tin, tout and ancestor[u][0]
     void dfs(int u, int p)
     {
         tin[u] = t++;
-        ancestor[u][0] = p;
+        ancestor[u][0] = p; // u's 1st ancestor is p 
 
-        for (int v : adj[u]) if (v != p)
-            dfs(v, u);
+        for (int v: adj[u])
+            if(v != p)
+                dfs(v, u);
         
         tout[u] = t++;
     }
-
+    
+    // returns true if u is v's ancestor
     bool is_ancestor(int u, int v)
     {
         return tin[u] <= tin[v] and tout[u] >= tout[v];
     }
 
-    // lower common ancestor
+    // return (lower common ancestor) lca(u, v) in O(log(n))
     int lca(int u, int v)
     {
         if(is_ancestor(u, v)) return u;
@@ -43,6 +46,7 @@ struct Binary_Lifting
         return ancestor[u][0];
     }
 
+    // returns kth ancestor of 'u' in O(log(n))
     int kth(int u, int k)
     {
         for(int i = l-1 ; i >= 0 ; i--)
@@ -58,18 +62,23 @@ struct Binary_Lifting
         return u;
     }
 
-    Binary_Lifting(int root)
+    /*
+        Preprocess the tree rooted in `root`
+        O(n*log(n))
+    */
+    void preprocess(int root) 
     {
         n = adj.size();
         tin.resize(n);
         tout.resize(n);
 
         t = 0;
-        l = ceil(log2(n)) + 2;
+        l = ceil(log2(n))+1;
+
         ancestor.assign(n, vector<int>(l, -1));
         dfs(root, -1);
         
-        // dp to calculate uuper ancestors
+        // dp to calculate uper ancestors
         for(int i = 1 ; i < l ; i++)
             for(int u = 0 ; u < n ; u++) if(ancestor[u][i-1] != -1)
                 ancestor[u][i] = ancestor[ancestor[u][i-1]][i-1];
